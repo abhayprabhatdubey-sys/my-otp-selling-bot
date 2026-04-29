e4246623db1deb13a1fb7bb2a4db2cfdb9ab62e0
abhayprabhatdubey-sys <abhayprabhatdubey@gmail.com>
2026-04-29 21:52:13 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,18 @@
+from flask import Flask
+from threading import Thread
+import os
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is Alive!"
+
+def run():
+    # Render automatically sets the PORT environment variable
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
